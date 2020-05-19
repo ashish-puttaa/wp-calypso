13faa417fec69e226cad1187f5a82d21f8944ac1
Marek Hrabe <marekhrabe@me.com>
2020-05-19 10:59:47 +0200

    add pending comment count to site menu in sidebar

--- client/layout/sidebar/item.jsx
+++ client/layout/sidebar/item.jsx
@@ -13,6 +13,7 @@ import { isFunction } from 'lodash';
  */
 import { isExternal } from 'lib/url';
 import MaterialIcon from 'components/material-icon';
+import Count from 'components/count';
 import { preload } from 'sections-helper';
 import TranslatableString from 'components/translatable/proptype';
 
@@ -20,7 +21,7 @@ export default function SidebarItem( props ) {
 	const isExternalLink = isExternal( props.link );
 	const showAsExternal = isExternalLink && ! props.forceInternalLink;
 	const classes = classnames( props.className, props.tipTarget, { selected: props.selected } );
-	const { materialIcon, materialIconStyle, icon, customIcon } = props;
+	const { materialIcon, materialIconStyle, icon, customIcon, count } = props;
 
 	let _preloaded = false;
 
@@ -67,6 +68,7 @@ export default function SidebarItem( props ) {
 				<span className="sidebar__menu-link-text menu-link-text" data-e2e-sidebar={ props.label }>
 					{ props.label }
 				</span>
+				{ count && <Count count={ count } /> }
 				{ showAsExternal && <Gridicon icon="external" size={ 24 } /> }
 				{ props.children }
 			</a>
@@ -89,4 +91,5 @@ SidebarItem.propTypes = {
 	forceInternalLink: PropTypes.bool,
 	testTarget: PropTypes.string,
 	tipTarget: PropTypes.string,
+	count: PropTypes.number,
 };
